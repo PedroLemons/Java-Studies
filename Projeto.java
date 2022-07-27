5334d26b34a17c6d3c13905fee8ac9c8ff946cb2
Pedro Lemos <p.a.lemos1@gmail.com>
2022-07-27 17:47:44 -0300

    Add files via upload

---
+++ Projeto.java
@@ -0,0 +1,47 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class Projeto {
+
+    public static void main(String[] args) throws Exception {
+        
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://api.opendota.com/api/heroes";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+        // System.out.println(HttpClient.newHttpClient().send(HttpRequest.newBuilder(URI.create("https://imdb-api.com/en/API/Top250Movies/k_kbldo5ps")).GET().build(), BodyHandlers.ofString()).body());
+
+        // separar somente os dados que nos interessam (título, imagem e nota)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> heroList = parser.parse(body);
+        // System.out.println(heroList.size());
+
+        // exibir e manipular os dados
+        // for (Map<String,String> hero : heroList) {
+
+        //     String heroName = hero.get("name");
+        //     String localizedName = hero.get("localized_name");
+
+        //     String urlImagem = "https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/antimage.png";
+
+        //     InputStream inputStream = new URL(urlImagem).openStream();
+        //     String nomeArquivo = heroName + "_sticker.png";
+
+        //     var generator = new StickerGen();
+        //     generator.cria(inputStream, nomeArquivo);
+
+        //     System.out.println(hero.get("name"));
+        // }
+    }
+}
---
+++ StickerGen.java
@@ -0,0 +1,55 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+
+import javax.imageio.ImageIO;
+import javax.swing.plaf.FontUIResource;
+
+public class StickerGen {
+
+    public void cria (InputStream inputStream, String nomeArquivo) throws Exception {
+
+        // leitura de imagem
+        // ?????????
+        // URL inputStream = new URL("https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/dark_willow.png");
+        // ?????????
+        // InputStream inputStream = new URL("https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/dark_willow.png").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria nova imagem em memoria com transparencia e com tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        BufferedImage novaImagem = new BufferedImage(largura, altura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original para nova imagem (em memoria)
+        Graphics2D g = (Graphics2D) novaImagem.getGraphics();
+        g.drawImage(imagemOriginal, 0, 0, null);
+
+        // estilizar a fonte
+        var fonte = new FontUIResource(Font.SANS_SERIF, Font.BOLD, 144);
+        g.setColor(Color.RED);
+        g.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        Projeto projeto = new Projeto();
+        String topText = projeto.localizedName;
+        String bottomText = "DOTA 4";
+        int posX = 740, posY = 180;
+        FontMetrics fm = g.getFontMetrics();
+        g.drawString(topText, posX - fm.stringWidth(topText)/2, posY );
+        g.drawString(bottomText, posX - fm.stringWidth(bottomText)/2, altura - 80 );
+
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+    }
+
+    // public static void main(String[] args) throws Exception {
+    //     var gen = new StickerGen();
+    //     gen.cria();
+    // }
+    
+}
